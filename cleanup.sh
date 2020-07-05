45c149801b09acda9124f51453436250a79f8fbc
Paul Hutchings <36493057+paulhutchings@users.noreply.github.com>
2020-07-05 11:36:15 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,4 @@
 #!/bin.bash
 
 # cleans up the 404 page if needed
-mv -f public/my404/index.html public/404.html \
-&& rm -r public/my404 \ 
-|| echo '404 correction not needed'
+mv -f public/my404/index.html public/404.html && rm -r public/my404 || echo '404 correction not needed'
